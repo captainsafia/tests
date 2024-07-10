af707af48c653fab0e729c19070ca2ecab16536a
Safia Abdalla <safia@safia.rocks>
2024-07-10 13:06:24 -0700

    Add sample for additional props handling in JsonSchemaExporter

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using System.Text.Json.Schema;
+using System.Text.Json.Serialization;
+
+var extendsDictionarySchema = JsonSchemaExporter.GetJsonSchemaAsNode(JsonSerializerOptions.Default.GetTypeInfo(typeof(ExtendsDictionary)));
+Console.WriteLine(extendsDictionarySchema.ToJsonString());
+
+var typeWithExtensionDataSchema = JsonSchemaExporter.GetJsonSchemaAsNode(JsonSerializerOptions.Default.GetTypeInfo(typeof(TypeWithExtensionData)));
+Console.WriteLine(typeWithExtensionDataSchema.ToJsonString());
+
+class ExtendsDictionary : Dictionary<string, Guid>
+{
+    public required string Name { get; set; }
+    public required bool IsEnabled { get; set; }
+}
+
+class TypeWithExtensionData
+{
+    public required string Name { get; set; }
+    public required bool IsEnabled { get; set; }
+
+    [JsonExtensionData]
+    public Dictionary<string, object>? AdditionalData { get; set; }
+
+}
